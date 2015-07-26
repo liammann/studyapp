702e57e24d04c08ac5f50ac8489b295248d550c5
Liam Mann <liam@liammann.com>
2015-07-26 21:47:25 +0100

    started working on the filter between units

--- www/app.js
+++ www/app.js
@@ -11,14 +11,13 @@ requirejs.config({
     }
 });
 
-require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html', 'libs/text!Task.html'], function (headerTpl, homeTpl, footerTpl, TaskTpl) {
+require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html', 'libs/text!Task.html', 'libs/text!sidebar.html'], function (headerTpl, homeTpl, footerTpl, TaskTpl, sidebarTpl) {
 	
 	var ApplicationRouter = Backbone.Router.extend({
 		routes: {
-			"": "home",
+			"": "tasks",
 			// "*actions": "home",
-			"task/:id": "task",
-			"task": "tasks"
+ 			":id": "unit"
 		},
 		initialize: function() {
 			this.headerView = new HeaderView();
@@ -26,23 +25,24 @@ require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'
 			this.footerView = new FooterView();
 			this.footerView.render();
 		},
-		home: function() {
+		unit: function() {
+			this.sidebarView = new SidebarView();
+			this.sidebarView.render();
 
-
-			this.homeView = new HomeView();
-			this.homeView.render();
+			this.unitView = new UnitView();
+			this.unitView.render();
 		},
-		task: function(id) {
+		// task: function(id) {
  
-			this.taskView = new TasksView();
-			this.taskView.render(id);
+		// 	this.taskView = new TasksView();
+		// 	this.taskView.render(id);
 
-		},
+		// },
 		tasks: function() {
- 
+ 			this.sidebarView = new SidebarView();
+			this.sidebarView.render();
 			this.taskView = new TasksView();
 			this.taskView.render();
-
 		}
 	});
 
@@ -67,73 +67,134 @@ require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'
 			this.$el.html(_.template(this.template));
 		}
 	});
-	HomeView = Backbone.View.extend({
-		el: "#content",
-		// template: "home.html",
-		template: homeTpl,
-		initialize: function() {
 
-		},
-		render: function() {
-			$(this.el).html(_.template(this.template));
-		}
-	});
-
-
-	var TaskModel = Backbone.Model.extend({
+	var UnitModel = Backbone.Model.extend({
 	  defaults: {
-	    name: 'topic 1',
-	    due: 0,
-	    child: ''
-	  },
-	  initialize: function(){
+	    name: 'unit 1',
+	    colour: 0
 	  }
 	});
-	TasksCollection = Backbone.Collection.extend({
-    	model: TaskModel, // Generally best practise to bring down a Model/Schema for your collection
-    	url: '/test/Tasks'
+	UnitsCollection = Backbone.Collection.extend({
+    	model: UnitModel, // Generally best practise to bring down a Model/Schema for your collection
+    	url: '/test/Units'
 	 });
 
-	TasksView = Backbone.View.extend({
-		el: "#content",
-		// template: "Task.html",
-		template: TaskTpl,
-		initialize: function() {
-		},
-		render: function(id) {
+	SidebarView = Backbone.View.extend({
+		el: "#sidebar",
+		template: sidebarTpl,
+		render: function() {
 			// $(this.el).html(_.template(this.template));
-
  			var that = this;
-      		var tasks = new TasksCollection({id: id});
-			console.log(tasks);
-      		tasks.fetch({
-      			success: function(tasks) {
-      		    $(that.el).html(_.template(that.template, {tasks: tasks.models, _:_}));
-        	}
-      	});
+      		var units = new UnitsCollection();
+      		units.fetch({
+				error: function(collection, response, options) {
+      				console.log('membersview fetch onerrorhandler');
+      				alert(response.responseText);
+  				},
+      			success: function(units) {
 
-
+      		    	$(that.el).html(_.template(that.template, {units: units.models, _:_}));
+    			}
+	      	});
 		}
 	});
 	
+	var TaskModel = Backbone.Model.extend({
+	    urlRoot: '/test/Tasks',
+
+	  	completed: function(attributes) {
+      		this.save(attributes, {patch: true});
+    	}
+	});
+	
+	TasksCollection = Backbone.Collection.extend({
+    	model: TaskModel, // Generally best practise to bring down a Model/Schema for your collection
+    	url: '/test/Tasks',
+	});
+	var taskModel = new TaskModel();
+    var tasks = new TasksCollection({model: taskModel});
+
+	UnitView = Backbone.View.extend({
+		el: "#content",
+		template: sidebarTpl,
+		render: function() {
+			// $(this.el).html(_.template(this.template));
+			var that = this;
+
+			tasks.fetch({
+				error: function(collection, response, options) {
+					console.log('membersview fetch onerrorhandler');
+					alert(response.responseText);
+  				},
+				success: function(units) {
+					var unitsss = units.where({unit: "22"});
+					console.log(unitsss);
+
+					$(that.el).html(_.template(that.template, {units: unitsss.models, _:_}));
+    			}
+			});
+		}
+	});
 
 	TasksView = Backbone.View.extend({
 		el: "#content",
+		model: taskModel,
+
 		// template: "Task.html",
 		template: TaskTpl,
-		initialize: function() {
-		},
-		render: function(id) {
-			// $(this.el).html(_.template(this.template));
 
+		initialize: function() {
+			this.first = [];
+			this.currentTarget = [];
+			this.model.bind("change", this.render);
+		},
+		events: {
+  			'click .notes-btn':'slideDown',
+  			'click .sub-btn':'slideDown',
+  			'click .hours-btn':'slideDown',
+  			'click .completed':'completed'
+		},
+		completed: function(evt) {
+			console.log(evt.target.parentNode.parentNode.attributes[0].value);
+        	var model = tasks.get(evt.target.parentNode.parentNode.attributes[0].value);
+        	model.completed({
+        	    open: false,
+        	});
+    	},
+		slideDown: function(evt){
+
+			var id = evt.target.parentNode.parentNode.attributes[0].value;
+
+			if (this.currentTarget[id] ===  evt.target && !this.first[id]){
+				$("#"+evt.currentTarget.attributes[0].value).slideUp();
+				$(this.currentTarget[id]).removeClass("active");
+				$(this.currentTarget[id].parentNode).removeClass("open");
+				this.first[id] = true;
+			} else {
+	    		if ($(evt.target.parentNode).hasClass("open")) {
+					$("#"+this.currentTarget[id].attributes[0].value).slideUp();
+					$(this.currentTarget[id]).removeClass("active");
+					$("#"+evt.currentTarget.attributes[0].value).slideDown();
+					$(evt.target).addClass("active");
+				}else{
+					$(evt.target.parentNode).addClass("open");
+					$("#"+evt.currentTarget.attributes[0].value).slideDown();
+					$(evt.target).addClass("active");
+				}
+				this.currentTarget[id] = evt.target;
+				this.first[id] = false;
+			}
+
+		},
+		render: function() {
  			var that = this;
-      		var tasks = new TasksCollection({id: id});
-			console.log(tasks);
-      		tasks.fetch({
-      			success: function(tasks) {
-      		    $(that.el).html(_.template(that.template, {tasks: tasks.models, _:_}));
-        	}
-      	});
+ 			// $(that.el).html(_.template(that.template, {tasks: tasks.models, _:_}));
+ 
+       		tasks.fetch({
+  				success: function(tasks) {
+      			    $(that.el).html(_.template(that.template, {tasks: tasks.models, _:_}));
+	        	}
+	      	});
 		}
 	});
 	
